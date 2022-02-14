94881f19e755774c53e5e147792ab36833b7cc4b
TheMaksoo <39918032+TheMaksoo@users.noreply.github.com>
2022-02-14 12:55:17 +0100

    added comments

--- PyReg/PyReg.py
+++ PyReg/PyReg.py
@@ -1,5 +1,5 @@
 import os
-
+# Start programma, stel de nodige variabelen in
 print ("========== PyReg ===========")
 print ("Welkom bij PyReg, het Python KassaSysteem voor en door DeveloperLand!")
 print ("Tel de kassa, en geef op hoeveel er nu in zit.")
@@ -8,16 +8,19 @@ keuze = 0
 dagTotaal = 0
 aantalBonnen = 0
 dagTotaalTerug = 0
-
+# Herhaal tot de gebruiker aangeeft dat het programma afgesloten moet worden
 while(not keuze == "9" ):
+    # Laat de gebruiker een keuze maken
     os.system('cls')
+    # Toon het hoofdmenu
     print ("======== HOOFDMENU =========")
+    # Gebruiker wil een nieuwe bon maken, stel variabelen voor bon in en toon menu
     print ("1. Nieuwe bon")
     print ("2. Retour")
     print ("3. Toon kassatotaal")
     print ("9. Afsluiten")
     print ("============================")
-
+    # Vraag gebruiker om keuze te maken
     keuze = input("Maak uw keuze en druk op <ENTER>.")
 
     if (keuze == "1"):
@@ -25,13 +28,20 @@ while(not keuze == "9" ):
         bonTotaal = 0
         bonString = ""
         while(not bestelKeuze == "9"):
+            # Toon bon-menu
             print ("========= BON MENU =========")
             print ("Bon "+ str(aantalBonnen))
+            # Voeg één volwasseneticket toe aan de bon
             print( "1. Volwassene                     € 19,-")
+            # Voeg één kinderkaartje toe aan de bon
             print( "2. Kinderen tot 12jr              € 9,-")
+            # Voeg een familiepas toe aan de bon
             print( "3. Familiepas (2x volw. 3x kind)  € 49")
+            # Voeg een parkkaart toe aan de bon
             print( "4. DeveloperLand-kaart            € 4,50")
+            # Voeg één volwasseneticket toe aan de bon
             print( "5. Kinderwagen/bolderkar (1 dag)  € 6")
+            # Voeg een parkeerkaart toe aan de bon
             print( "6. Parkeerdagkaart                € 9")
             print( "9. Afronden bon")
             print( "Z. Bon annuleren")
@@ -60,6 +70,7 @@ while(not keuze == "9" ):
                 aantalBonnen = aantalBonnen +1
                 dagTotaal = dagTotaal + bonTotaal
                 print( bonString )
+                # Bon is klaar, toon totaal en reken af
                 print ("======== BON TOTAAL ========")
                 print( "Te betalen: " + str(bonTotaal) )
                 betaald = float(input("Betaald:    "))
@@ -67,31 +78,41 @@ while(not keuze == "9" ):
                 print ( "Terug:     " + str(terug))
                 input("Druk op <ENTER> om door te gaan.")
             elif (bestelKeuze == "Z" or bestelKeuze == "z"):
+                # Gebruiker wil de bon annuleren; reset bon variabelen en keer terug naar het hoofdmenu
                 bestelKeuze = 9
                 bonTotaal = 0
                 bonString = ""
     elif(keuze == "2"):
+        # Gebruiker wil bedrag retourneren
         print ("Uitvoeren terugbetaling")
+        # Vraag om bedrag en reden
         terugTeGeven = float(input( "Bedrag originele bon: "))
         reden = input("Reden retour: ")
+        # Verwerk retourbedrag in dagtotaal
         dagTotaalTerug = terugTeGeven
     elif(keuze == "3"):
+        # Gebruiker wil de dagtotalen zien
         print ("======= DAG TOTALEN ========")
         print ("In kassa begin:   " + str(bedragInKassaBegin))
         print ("Verkocht:         " + str(dagTotaal))
         print ("Retour:           " + str(dagTotaalTerug))
         print ("In kassa:         " + str( bedragInKassaBegin + dagTotaal - dagTotaalTerug ))
         input("Druk op <ENTER> om door te gaan.")
+# Gebruiker wil het programma afsluiten, vraag om bedrag in kassa
 inKassa = float(input("Hoeveel zit er nu in de kassa?")) 
 while(not inKassa == (bedragInKassaBegin + dagTotaal - dagTotaalTerug )):
     print("Je hebt een kassaverschil! Tel de kassa opnieuw")
     inKassa = float(input("Hoeveel zit er nu in de kassa?")) 
 
 os.system('cls')
+# Herhaal zolang het opgegeven bedrag in kassa niet overeenkomt met het berekende bedrag in kassa
 print("Kassa klopt, programma wordt afgesloten.")
+# Kassa is correct afgesloten, toon dagtotalen
 print ("======== DAGTOTALEN ========")
 print ("Aantal bonnen: " + str(aantalBonnen))
+# Kassa is correct afgesloten, toon dagtotalen
 print ("Verkocht:      " + str(dagTotaal))
+# Toon retourbedrag
 print ("Totaal retour: " + str(dagTotaalTerug))
 print ("In kassa:      " + str(inKassa))
 print ("============================")
